aed36cbbad2035fbedfab32830f924f4640fe7e2
bestie06 <fatih.kgokmen@gmail.com>
2020-12-18 02:37:28 -0500

    updated with css and added work router

--- app.js
+++ app.js
@@ -2,9 +2,11 @@ const express = require("express");
 const bodyParser = require("body-parser");
 
 const app = express();
-var items=[];
+var items=["book","study"];
+var workItems=[];
 app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({extended:true}));
+app.use(express.static("public"));
 
 
 app.get("/", function(req, res) {
@@ -18,16 +20,31 @@ app.get("/", function(req, res) {
 
   var day=today.toLocaleDateString("en-US",options);
 
-  res.render("list", {kindOfDay: day,newListItem: items});
+  res.render("list", {listTitle: day,newListItems: items});
 });
 
+
+
 app.post("/",function(req,res){
-   var item = req.body.newItem
+
+  if (req.boy.list ==="work"){
+    
+  }
+   var item = req.body.newItem;
    items.push(item);
    res.redirect("/")
 
 });
+app.get("/work", function(req,res){
+  res.render("list",{listTitle:"Work List",newListItems:workItems});
+})
 
+app.post("/work",function(req,res){
+  var item=req.body.newItem;
+  workItems.push(item);
+  res.redirect("/work");
+
+})
 app.listen(3000, function() {
   console.log("Server started on port 3000");
 });
